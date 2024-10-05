d453cdcd9c5046e2599195f072b4f3c8c4702e49
Gissel-coder <gissel.lesme01@gmail.com>
2024-10-05 00:18:01 -0400

    Desafio DOM

--- app.js
+++ app.js
@@ -19,23 +19,26 @@ textoAzul.textContent = azul;
 
 // Función para actualizar el color de fondo de la página.
 function actualizarColor(rojo, verde, azul) {
-  // Completa esta función para cambiar el color de fondo.
+  document.body.style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`;
 }
 
 // Agrega un event listener para el input de Rojo.
-inputRojo.addEventListener('change', (e) => {
-  // Actualiza el valor de la variable y el texto.
-  // Llama a la función para actualizar el color de fondo.
+inputRojo.addEventListener('input', (e) => {
+  rojo = e.target.value; // Actualiza el valor de la variable
+  textoRojo.textContent = rojo; // Muestra el valor en el <p>
+  actualizarColor(rojo, verde, azul); // Llama a la función para actualizar el color de fondo
 });
 
 // Agrega un event listener para el input de Verde.
-inputVerde.addEventListener('change', (e) => {
-  // Actualiza el valor de la variable y el texto.
-  // Llama a la función para actualizar el color de fondo.
+inputVerde.addEventListener('input', (e) => {
+  verde = e.target.value; // Actualiza el valor de la variable
+  textoVerde.textContent = verde; // Muestra el valor en el <p>
+  actualizarColor(rojo, verde, azul); // Llama a la función para actualizar el color de fondo
 });
 
 // Agrega un event listener para el input de Azul.
-inputAzul.addEventListener('change', (e) => {
-  // Actualiza el valor de la variable y el texto.
-  // Llama a la función para actualizar el color de fondo.
+inputAzul.addEventListener('input', (e) => {
+  azul = e.target.value; // Actualiza el valor de la variable
+  textoAzul.textContent = azul; // Muestra el valor en el <p>
+  actualizarColor(rojo, verde, azul); // Llama a la función para actualizar el color de fondo
 });
